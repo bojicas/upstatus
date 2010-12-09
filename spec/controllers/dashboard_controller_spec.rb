6ecf2dacbf19834cddde27dc77b4d18ff48e552f
Silviu D. Bojica <bojicas@me.com>
2010-12-09 23:47:39 +0400

    Revisited DashboardController spec. Wrote more useful test cases

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,10 +3,33 @@ require 'spec_helper'
 describe DashboardController do
 
   describe "GET 'index'" do
-    it "should be successful" do
+    before do
+      @services = mock_model(Service)
+      Service.stub(:all).and_return(@services)
+    end
+
+    def do_get
       get 'index'
-      response.should render_template(:index)
+    end
+
+    it "should be successful" do
+      do_get
       response.should be_success
+    end
+
+    it "should render index template" do
+      do_get
+      response.should render_template(:index)
+    end
+
+    it "should find all services" do
+      Service.should_receive(:all).and_return(@services)
+      do_get
+    end
+
+    it "should assign found services for the view" do
+      do_get
+      assigns[:services].should == @services
     end
   end
 
