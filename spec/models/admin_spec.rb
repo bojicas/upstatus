764e4039732cc7fda9c2d18cd0fe51e7a60392d5
Silviu D. Bojica <bojicas@me.com>
2010-12-10 11:30:34 +0400

    Added spec to the Admin model. Admin is already tested, but I include some just to make sure :validatable attribute is in place

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Admin do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @admin = Admin.new(
+      :email => 'admin@cubicleapps.com',
+      :password => 'secretpass',
+      :password_confirmation => 'secretpass'
+    )
+  end
+
+  it "is valid with valid attributes" do
+    @admin.should be_valid
+  end
+
+  it "is not valid without email" do
+    @admin.email = nil
+    @admin.should_not be_valid
+  end
+
+  it "is not valid without password" do
+    @admin.password = nil
+    @admin.should_not be_valid
+  end
+
+  it "is not valid with password less than 6 characters" do
+    @admin.password = "passw"
+    @admin.should_not be_valid
+  end
 end
